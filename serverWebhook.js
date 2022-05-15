11d2dcc546f947ba7025bf2324311774b018950b
Du Guowei <duguowei1000@gmail.com>
2022-05-16 03:36:19 +0800

    testWebhook-31

--- serverWebhook.js
+++ serverWebhook.js
@@ -58,7 +58,7 @@ bot.telegram.setWebhook(`${DOMAIN}`).then(() => {
 
 app.get('/', (req, res) => res.send('Hello World_yesyesyo!'))
 
-app.use(bot.webhookCallback(`/AAHK`))
+app.use(bot.webhookCallback(`/aahk`))
 app.post(`/`, (req, res) => {
   // console.log("req.body",req.body)
   // res.send("Ok");
